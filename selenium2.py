5913bb99d6b72648529f03a898c3906d7703b867
ahndavid90 <40782362+ahndavid90@users.noreply.github.com>
2020-03-31 15:03:39 -0400

    Update selenium2.py

--- selenium2.py
+++ selenium2.py
@@ -7,9 +7,6 @@ import math
 options = Options()
 options.headless = True
 
-def Sort(element):
-    return element[1]
-
 country_info_list = []
 
 chromedriver_path = os.getcwd() + '/chromedriver'
@@ -17,8 +14,8 @@ chromedriver_path = os.getcwd() + '/chromedriver'
 driver = webdriver.Chrome(executable_path = chromedriver_path, options = options)
 driver.get('https://www.worldometers.info/coronavirus/')
 
-for country in driver.find_elements_by_xpath("//tr[@class='odd' or @class='even']"):
-    country_info= []
+for country in driver.find_elements_by_xpath("//tr[@class = 'odd' or @class = 'even']"):
+    country_info = []
     location = country.find_elements_by_css_selector('td')[0].text
     exposure = country.find_elements_by_css_selector('td')[1].text
     deaths = country.find_elements_by_css_selector('td')[3].text
@@ -34,12 +31,13 @@ for country in driver.find_elements_by_xpath("//tr[@class='odd' or @class='even'
         deaths = 0
     elif ',' in deaths:
         deaths = int("".join(deaths.split(",")))
+
     else:
         deaths = int(deaths)
 
     if deaths > 10 and exposure > 500:
         m_x = deaths/exposure
-        q_x = round(1 - math.exp(-m_x), 4)
+        q_x = round(1-math.exp(-m_x), 4)
         country_info.append(location)
         country_info.append(q_x)
 
@@ -50,8 +48,9 @@ country_info_list.sort(key = lambda x: x[1], reverse = True)
 
 print('')
 print("Today's Date: {}".format(date.today()))
-print('----')
+print('')
 for element in country_info_list[0:10]:
     print(*element)
+
 
 
